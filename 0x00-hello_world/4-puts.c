e7c788e82bf6b1badc963b1e311232666f8e6b49
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-08 19:43:54 -0700

    Using the puts function in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point 
- * description - Using the puts function instead of printf
+ * Description - Using the puts function instead of printf
  * Return: 0 (success)
-*/
+ */
 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 }
