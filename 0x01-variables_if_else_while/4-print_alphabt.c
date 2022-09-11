dd91bf9ff76558eb90cdc1bd8af59c6fb49bf8c5
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-10 17:55:01 -0700

    Missing Alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Using the continue function to exclude q&e from the alphabet
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'q' && x == 'e')
+		{
+			putchar(x);
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+		
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -4,4 +4,4 @@ TASKS ON VARIABLES, IF, ELSE IF, AND WHILE LOOP
 1. Getting accustomed to the if, else if and else function.
 2. Using the putchar function to print a to z in lower case.
 3. Printing the alphabet with putchar in lower and upper case.
-
+4. Using the continue function to elimnate some alphabet in the Alphabets
