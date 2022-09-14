fe14fc9b8f31f565288f7eb9063bd8d703501889
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-13 20:03:22 -0700

    Calling the function from the header file main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: calling the function in the header file and printing a-z
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	/* Decleration */
+	int a;
+
+	/* Statement */
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 Functions and nested loops
 1. Writing a program to print _putchar.
+2. Creating an header file and printing the alphabet in lowercase
