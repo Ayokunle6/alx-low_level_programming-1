e33948a4ed05008105fb20944f7ad1119f89f86c
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-13 19:35:55 -0700

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: print out "_putchar"
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Functions and nested loops
+1. Writing a program to print _putchar.
