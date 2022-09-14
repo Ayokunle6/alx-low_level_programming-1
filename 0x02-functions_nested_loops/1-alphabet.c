dba41f48e8f52562a7ca23f31a6bf4265934ef8f
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-14 16:25:13 -0700

    Calling the function from the header file main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point(prints alphabet in lowercase)
  * Description: calling the function in the header file and printing a-z
  * Return: 0 (success)
  */
 
 void print_alphabet(void)
 {
-	
 	/* Decleration */
 	int a;
 
