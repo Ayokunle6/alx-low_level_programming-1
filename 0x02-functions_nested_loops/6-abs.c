d4d6ba84fbc5f05b30fd0642da22e5702774374e
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-14 18:26:19 -0700

    Absolute values of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,6 +5,7 @@
 /**
  * _abs - Entry point(Calling the function from main.h file)
  * Description: The function computes the absolute value of a number
+ * @num: variable for the integer
  * Return: 0 (success)
  */
 
