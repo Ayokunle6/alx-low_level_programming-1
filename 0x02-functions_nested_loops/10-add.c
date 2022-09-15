bc2239253da2448de9bc44e5a223bccf9ce0506d
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-14 21:38:20 -0700

    summation

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,12 +5,15 @@
 /**
  * add - Entry point(calling function from main.h file)
  * Description: Summing two integers together
- * @a, @b: variables for the integers
+ * @a: variable for integer
+ * @b: variable for integer
  * Return: 0 (success)
  */
 
 int add(int a, int b)
 {
 	int sum = (a + b);
+
 	return (sum);
+
 }
