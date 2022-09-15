446a2d7908e1b7b6076cad62316c23b0d311ab01
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-14 17:07:26 -0700

    confirming case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _islower -Entry point (check for lower case)
+ * Description: calling function from main.h and check for lower case
+ * Return: 0 (success)
+ */
+
+int _islower(int c)
+{
+	/* Statement */
+
+	if (c == 'a' && c == 'z')
+	{
+		return (1);
+	}
+	else
+		return(0);
+}
