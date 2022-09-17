f311973485c2f77c28f4e260dd208e7000805a6f
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-16 19:38:17 -0700

    printing 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,13 +4,13 @@
 /**
  * more_numbers - prints numbers
  * Description: prints the numbers from 0 to 14 ten times
- * Return: success
  */
 
 void more_numbers(void)
 {
 	/* Declerations */
-	int i, j;
+	int i;
+	int j;
 
 	/* Statement */
 	for (i = 0; i <= 10; i++)
