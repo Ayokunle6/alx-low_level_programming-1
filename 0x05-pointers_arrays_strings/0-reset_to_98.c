14820e32349732aa318337bfce53facbf4557572
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-19 20:37:38 -0700

    Dereferencing process

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - Function that updates the value it points to
  * @n: A pointer to the integer
- * Description: A function that that takes a 
+ * Description: A function that that takes a
  * pointer to an int as parameter and updates the value it points to to 98.
  */
 
