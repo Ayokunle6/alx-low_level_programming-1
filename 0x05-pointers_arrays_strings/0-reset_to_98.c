77bc9f76768518ab7246233f6e6e0b22199d1ea5
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-19 20:35:19 -0700

    Dereferencing process

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that updates the value it points to
+ * @n: A pointer to the integer
+ * Description: A function that that takes a 
+ * pointer to an int as parameter and updates the value it points to to 98.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+C - Pointers, arrays and strings
+0. Creating a function that takes a pointer to an int as parameter and updates the value it points to 98 by using the Dereferencing process
+1. 
