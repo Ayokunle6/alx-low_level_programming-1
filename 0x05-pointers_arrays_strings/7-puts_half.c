067e4347049649cd7d2b35efce8594eaefea59c6
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-20 21:04:00 -0700

    Half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,13 +15,11 @@ void puts_half(char *str)
 
 	/*Statement*/
 	while (str[i] != '\0')
-	{
 		i++;
-	}
 	n = i / 2;
 	if (i % 2 != 0)
 		n = ((i - 1) / 2) + 1;
-	for (n = 0; n < i; n++)
+	for (; n < i; n++)
 		_putchar(str[n]);
 
 	_putchar('\n');
