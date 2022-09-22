d2a8ee1e116d3c620396d74bd8a6e87f3b016ab1
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-21 17:56:12 -0700

    i lo

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: Variable that receive first string
+ * @src: Variable that receive the second string
+ * Return: success
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	/*Decleration*/
+	int len1, len2;
+
+	len1 = 0;
+	len2 = 0;
+
+
+	/*Statement*/
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+C - More pointers, arrays and strings
+0. Creating a function that concatenate two strings.
+
