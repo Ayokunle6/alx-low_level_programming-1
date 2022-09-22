255ddb6326987ff7cfda6735b1a1611608b30ddb
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-21 21:03:47 -0700

    Concatenating strings without strcat function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,23 +10,20 @@
 char *_strcat(char *dest, char *src)
 {
 	/*Decleration*/
-	int len1, len2;
-	char first, sec;
+	int len1, len2, i;
 
 	len1 = 0;
 	len2 = 0;
 
-
 	/*Statement*/
-	while (len1 != '\0')
+	while (dest[len1] != '\0')
 	{
 		len1++;
 	}
-	while (len2 != '\0')
+	while (src[len2] != '\0')
 		len2++;
 
-	first = dest[len1];
-	sec = src[len2];
-	_putchar(first + sec);
+	for (i = 0; i <= len2; i++)
+	       dest[len1 + i] = src[i];
 	return (dest);
 }
