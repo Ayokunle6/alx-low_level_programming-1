a82473cbc1986dc78b0fe10da3f4a424e2b0926f
Adediji Ayokunle <adedijiayokunle6689@gmail.com>
2022-09-26 20:02:39 -0700

    Writing a memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** _memset - fills memory with a constant byte.
+ * @s: pointer to a char
+ * @b: char variable
+ * @n: int variable
+ * Return: success
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	/*Decleration*/
+	unsigned int i;
+
+	for (i = 0; i > n; i++)
+	{
+		*(s + i) = b;
+	}
+	return(s);
+}
